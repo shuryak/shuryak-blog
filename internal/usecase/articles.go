322d80f0fe380da0d5db9d98b067f828f113e784
Alexander Konovalov <shuryak@protonmail.com>
2022-10-21 16:37:06 +0300

    feat(articles-usecase): all according repo

--- internal/usecase/articles.go
+++ internal/usecase/articles.go
@@ -17,10 +17,12 @@ func New(r ArticlesRepo) *ArticlesUseCase {
 	return &ArticlesUseCase{r}
 }
 
+// TODO: deal with the context
+
 func (uc *ArticlesUseCase) Create(ctx context.Context, a entity.Article) (*entity.Article, error) {
 	id, err := uc.repo.Create(context.Background(), a)
 	if err != nil {
-		return nil, fmt.Errorf("ArticlesUseCase - Create - s.repo.Store: %w", err)
+		return nil, fmt.Errorf("ArticlesUseCase - Create - s.repo.Create: %w", err)
 	}
 
 	articleEntity, err := uc.repo.GetById(context.Background(), id)
@@ -31,11 +33,38 @@ func (uc *ArticlesUseCase) Create(ctx context.Context, a entity.Article) (*entit
 	return articleEntity, nil
 }
 
+func (uc *ArticlesUseCase) GetById(ctx context.Context, id uint) (*entity.Article, error) {
+	a, err := uc.repo.GetById(context.Background(), id)
+	if err != nil {
+		return nil, fmt.Errorf("ArticlesUseCase - GetById - s.repo.GetById")
+	}
+
+	return a, nil
+}
+
 func (uc *ArticlesUseCase) GetMany(ctx context.Context, offset uint, count uint) ([]entity.Article, error) {
-	articles, err := uc.repo.GetMany(ctx, offset, count)
+	a, err := uc.repo.GetMany(context.Background(), offset, count)
 	if err != nil {
 		return nil, fmt.Errorf("ArticlesUseCase - GetMany - s.repo.GetMany: %w", err)
 	}
 
-	return articles, nil
+	return a, nil
+}
+
+func (uc *ArticlesUseCase) Update(ctx context.Context, a entity.Article) (*entity.Article, error) {
+	updatedArticleEntity, err := uc.repo.Update(context.Background(), a)
+	if err != nil {
+		return nil, fmt.Errorf("ArticlesUseCase - Update - s.repo.Update: %w", err)
+	}
+
+	return updatedArticleEntity, nil
+}
+
+func (uc *ArticlesUseCase) Delete(ctx context.Context, id uint) (*entity.Article, error) {
+	deletedArticleEntity, err := uc.repo.Delete(context.Background(), id)
+	if err != nil {
+		return nil, fmt.Errorf("ArticlesUseCase - Delete - s.repo.Delete: %w", err)
+	}
+
+	return deletedArticleEntity, nil
 }
--- internal/usecase/interfaces.go
+++ internal/usecase/interfaces.go
@@ -9,7 +9,10 @@ type (
 	// Article - .
 	Article interface {
 		Create(ctx context.Context, a entity.Article) (*entity.Article, error)
+		GetById(ctx context.Context, id uint) (*entity.Article, error)
 		GetMany(ctx context.Context, offset uint, count uint) ([]entity.Article, error)
+		Update(ctx context.Context, a entity.Article) (*entity.Article, error)
+		Delete(ctx context.Context, id uint) (*entity.Article, error)
 	}
 
 	ArticlesRepo interface {
