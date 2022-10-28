157611ef9f6685031eda88e264ee2478629b54a9
Alexander Konovalov <shuryak@protonmail.com>
2022-10-28 15:12:49 +0300

    feat(internal-controller): full grpc endpoints coverage

--- services/articles/internal/controller/grpc/articles_grpc/articles_grpc.pb.go
+++ services/articles/internal/controller/grpc/articles_grpc/articles_grpc.pb.go
@@ -22,7 +22,11 @@ const _ = grpc.SupportPackageIsVersion7
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type ArticlesClient interface {
-	Create(ctx context.Context, in *CreateRequest, opts ...grpc.CallOption) (*CreateResponse, error)
+	Create(ctx context.Context, in *CreateRequest, opts ...grpc.CallOption) (*SingleArticleResponse, error)
+	GetById(ctx context.Context, in *ArticleIdRequest, opts ...grpc.CallOption) (*SingleArticleResponse, error)
+	GetMany(ctx context.Context, in *GetManyRequest, opts ...grpc.CallOption) (*MultipleArticlesResponse, error)
+	Update(ctx context.Context, in *UpdateRequest, opts ...grpc.CallOption) (*SingleArticleResponse, error)
+	Delete(ctx context.Context, in *ArticleIdRequest, opts ...grpc.CallOption) (*SingleArticleResponse, error)
 }
 
 type articlesClient struct {
@@ -33,9 +37,45 @@ func NewArticlesClient(cc grpc.ClientConnInterface) ArticlesClient {
 	return &articlesClient{cc}
 }
 
-func (c *articlesClient) Create(ctx context.Context, in *CreateRequest, opts ...grpc.CallOption) (*CreateResponse, error) {
-	out := new(CreateResponse)
+func (c *articlesClient) Create(ctx context.Context, in *CreateRequest, opts ...grpc.CallOption) (*SingleArticleResponse, error) {
+	out := new(SingleArticleResponse)
 	err := c.cc.Invoke(ctx, "/articlesGrpc.Articles/Create", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *articlesClient) GetById(ctx context.Context, in *ArticleIdRequest, opts ...grpc.CallOption) (*SingleArticleResponse, error) {
+	out := new(SingleArticleResponse)
+	err := c.cc.Invoke(ctx, "/articlesGrpc.Articles/GetById", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *articlesClient) GetMany(ctx context.Context, in *GetManyRequest, opts ...grpc.CallOption) (*MultipleArticlesResponse, error) {
+	out := new(MultipleArticlesResponse)
+	err := c.cc.Invoke(ctx, "/articlesGrpc.Articles/GetMany", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *articlesClient) Update(ctx context.Context, in *UpdateRequest, opts ...grpc.CallOption) (*SingleArticleResponse, error) {
+	out := new(SingleArticleResponse)
+	err := c.cc.Invoke(ctx, "/articlesGrpc.Articles/Update", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *articlesClient) Delete(ctx context.Context, in *ArticleIdRequest, opts ...grpc.CallOption) (*SingleArticleResponse, error) {
+	out := new(SingleArticleResponse)
+	err := c.cc.Invoke(ctx, "/articlesGrpc.Articles/Delete", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -46,7 +86,11 @@ func (c *articlesClient) Create(ctx context.Context, in *CreateRequest, opts ...
 // All implementations must embed UnimplementedArticlesServer
 // for forward compatibility
 type ArticlesServer interface {
-	Create(context.Context, *CreateRequest) (*CreateResponse, error)
+	Create(context.Context, *CreateRequest) (*SingleArticleResponse, error)
+	GetById(context.Context, *ArticleIdRequest) (*SingleArticleResponse, error)
+	GetMany(context.Context, *GetManyRequest) (*MultipleArticlesResponse, error)
+	Update(context.Context, *UpdateRequest) (*SingleArticleResponse, error)
+	Delete(context.Context, *ArticleIdRequest) (*SingleArticleResponse, error)
 	mustEmbedUnimplementedArticlesServer()
 }
 
@@ -54,8 +98,20 @@ type ArticlesServer interface {
 type UnimplementedArticlesServer struct {
 }
 
-func (UnimplementedArticlesServer) Create(context.Context, *CreateRequest) (*CreateResponse, error) {
+func (UnimplementedArticlesServer) Create(context.Context, *CreateRequest) (*SingleArticleResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Create not implemented")
+}
+func (UnimplementedArticlesServer) GetById(context.Context, *ArticleIdRequest) (*SingleArticleResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetById not implemented")
+}
+func (UnimplementedArticlesServer) GetMany(context.Context, *GetManyRequest) (*MultipleArticlesResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetMany not implemented")
+}
+func (UnimplementedArticlesServer) Update(context.Context, *UpdateRequest) (*SingleArticleResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Update not implemented")
+}
+func (UnimplementedArticlesServer) Delete(context.Context, *ArticleIdRequest) (*SingleArticleResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Delete not implemented")
 }
 func (UnimplementedArticlesServer) mustEmbedUnimplementedArticlesServer() {}
 
@@ -88,6 +144,78 @@ func _Articles_Create_Handler(srv interface{}, ctx context.Context, dec func(int
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Articles_GetById_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ArticleIdRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ArticlesServer).GetById(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/articlesGrpc.Articles/GetById",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ArticlesServer).GetById(ctx, req.(*ArticleIdRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Articles_GetMany_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetManyRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ArticlesServer).GetMany(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/articlesGrpc.Articles/GetMany",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ArticlesServer).GetMany(ctx, req.(*GetManyRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Articles_Update_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UpdateRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ArticlesServer).Update(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/articlesGrpc.Articles/Update",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ArticlesServer).Update(ctx, req.(*UpdateRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Articles_Delete_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ArticleIdRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ArticlesServer).Delete(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/articlesGrpc.Articles/Delete",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ArticlesServer).Delete(ctx, req.(*ArticleIdRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Articles_ServiceDesc is the grpc.ServiceDesc for Articles service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -98,6 +226,22 @@ var Articles_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Create",
 			Handler:    _Articles_Create_Handler,
+		},
+		{
+			MethodName: "GetById",
+			Handler:    _Articles_GetById_Handler,
+		},
+		{
+			MethodName: "GetMany",
+			Handler:    _Articles_GetMany_Handler,
+		},
+		{
+			MethodName: "Update",
+			Handler:    _Articles_Update_Handler,
+		},
+		{
+			MethodName: "Delete",
+			Handler:    _Articles_Delete_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
